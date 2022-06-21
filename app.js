89b4493ecb93b1fab828853c4ad4a21b117817a1
solenm <solenm@gmail.com>
2022-06-21 14:50:25 -0400

    created javascript file

---
+++ app.js
@@ -0,0 +1,4 @@
+const sections= document.querySelectorAll('.section'); 
+const secBtns= document.querySelectorAll('.controls');
+const secBtn= document.querySelectorAll('.control');
+const allSec= document.querySelector('.main-content');
