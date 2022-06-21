f1232808d6bc649d40eb048c2f049345d0a4c205
solenm <solenm@gmail.com>
2022-06-21 19:33:24 -0400

    added animation and page transition functionality to buttons

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ function transitions(){
             this.className += ' active-btn';
         })
     }
+
+    allSec.addEventListener('click', (e) =>{
+        const id= e.target.dataset.id;
+        if(id){
+            secBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element= document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 transitions(); 
