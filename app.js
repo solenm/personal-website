8988946463c913c5168d3a8b62d9b1f778c06df4
solenm <solenm@gmail.com>
2022-06-27 11:33:37 -0400

    add functionality between toggling dark and lightmode

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ function transitions(){
             element.classList.add('active');
         }
     })
+
+    //Toggle theme
+    const themeBtn= document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () =>{
+        let element= document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 transitions(); 
